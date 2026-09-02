69bc6382a3545ea64436c88ff0ba60e557ec4e44
Akaza41 <raqeebyounas76@gmail.com>
2026-09-02 19:06:10 +0500

    feat(routes): support /internal/Farm and /internal/farm routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/internal/Farm",
+        destination: "/internal/farm",
+      },
+      {
+        source: "/internal/Form",
+        destination: "/internal/farm",
+      },
+      {
+        source: "/internal/form",
+        destination: "/internal/farm",
+      },
+    ];
+  },
   webpack: (config) => {
     config.cache = false;
     return config;
